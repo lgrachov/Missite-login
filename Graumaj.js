692f407010edce805d22c47f81ed4a3e954274f3
lgrachov <78234184+lgrachov@users.noreply.github.com>
2021-12-11 14:12:20 +0200

    Create Graumaj.js

---
+++ Graumaj.js
@@ -0,0 +1,801 @@
+if esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+endif esle
+var esle = 9,99,8
+esle esle
+var if 9,9,9,9
+end
